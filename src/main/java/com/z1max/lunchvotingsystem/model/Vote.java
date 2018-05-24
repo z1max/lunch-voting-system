e19ce4655f8ae63c9403f008178d6fa94281bf4d
z1max <forcazimaxqwe123>
2018-05-24 12:34:26 +0300

    Refactor, toString with processing lazy fields

--- src/main/java/com/z1max/lunchvotingsystem/model/Vote.java
+++ src/main/java/com/z1max/lunchvotingsystem/model/Vote.java
@@ -1,40 +1,46 @@
 package com.z1max.lunchvotingsystem.model;
 
-import java.time.LocalDateTime;
+import org.hibernate.Hibernate;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
 
 @Entity
+@Table(name = "votes")
 public class Vote extends AbstractBaseEntity {
 
     @NotNull
-    @OneToOne
-    @JoinColumn(name = "user_id")
-    private User user;
+    @Column(name = "user_id")
+    private Integer userId;
 
     @NotNull
-    @OneToOne
-    @JoinColumn(name = "restaurant_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "restaurant_id", nullable = false)
     private Restaurant restaurant;
 
     @NotNull
-    @Column(name = "date_ime")
-    private LocalDateTime dateTime;
+    @Column(name = "date")
+    private LocalDate date;
 
     public Vote(){}
 
-    public Vote(User user, Restaurant restaurant, LocalDateTime dateTime) {
-        this.user = user;
+    public Vote(Integer id, Integer userId, Restaurant restaurant, LocalDate date) {
+        super(id);
+        this.userId = userId;
         this.restaurant = restaurant;
-        this.dateTime = dateTime;
+        this.date = date;
     }
 
-    public User getUser() {
-        return user;
+    public Integer getUserId() {
+        return userId;
     }
 
-    public void setUser(User user) {
-        this.user = user;
+    public void setUserId(Integer userId) {
+        this.userId = userId;
     }
 
     public Restaurant getRestaurant() {
@@ -45,11 +51,22 @@ public class Vote extends AbstractBaseEntity {
         this.restaurant = restaurant;
     }
 
-    public LocalDateTime getDateTime() {
-        return dateTime;
+    public LocalDate getDate() {
+        return date;
     }
 
-    public void setDateTime(LocalDateTime dateTime) {
-        this.dateTime = dateTime;
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder(getClass().getName());
+        sb.append("{id=").append(id);
+        sb.append(", userId=").append(userId);
+        sb.append(", restaurant=").append(Hibernate.isInitialized(restaurant) ? restaurant.getId() : null);
+        sb.append(", date=").append(date);
+        sb.append('}');
+        return sb.toString();
     }
 }
