09181f35973137bda35179a957fcc3882111d221
z1max <forcazimaxqwe123>
2018-05-24 12:30:32 +0300

    Add votes, toString with processing lazy fields

--- src/main/java/com/z1max/lunchvotingsystem/model/Restaurant.java
+++ src/main/java/com/z1max/lunchvotingsystem/model/Restaurant.java
@@ -1,5 +1,7 @@
 package com.z1max.lunchvotingsystem.model;
 
+import org.hibernate.Hibernate;
+
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,16 +13,21 @@ public class Restaurant extends AbstractNamedEntity {
     @OneToMany(mappedBy = "restaurant", fetch = FetchType.LAZY)
     private List<Dish> menu;
 
+    @OneToMany(mappedBy = "restaurant", fetch = FetchType.LAZY)
+    private List<Vote> votes;
+
     public Restaurant() {}
 
     public Restaurant(Integer id, String name){
         super(id, name);
         this.menu = new ArrayList<>();
+        this.votes = new ArrayList<>();
     }
 
     public Restaurant(Integer id, String name, List<Dish> menu) {
         super(id, name);
         this.menu = menu;
+        this.votes = new ArrayList<>();
     }
 
     public List<Dish> getMenu() {
@@ -43,12 +50,33 @@ public class Restaurant extends AbstractNamedEntity {
         dish.setRestaurant(null);
     }
 
+    public List<Vote> getVotes() {
+        return votes;
+    }
+
+    public void setVotes(List<Vote> votes) {
+        this.votes = votes;
+    }
+
+    public void addVote(Vote vote){
+        votes.add(vote);
+        if (vote.getRestaurant() != this){
+            vote.setRestaurant(this);
+        }
+    }
+
+    public void removeVote(Vote vote){
+        votes.remove(vote);
+        vote.setRestaurant(null);
+    }
+
     @Override
     public String toString() {
-        final StringBuilder sb = new StringBuilder("Restaurant{");
-        sb.append("menu=").append(menu);
+        final StringBuilder sb = new StringBuilder(getClass().getName());
+        sb.append("{id=").append(id);
         sb.append(", name='").append(name).append('\'');
-        sb.append(", id=").append(id);
+        sb.append(", menu=").append(Hibernate.isInitialized(menu) ? menu : null);
+        sb.append(", votes=").append(Hibernate.isInitialized(votes) ? votes : null);
         sb.append('}');
         return sb.toString();
     }
