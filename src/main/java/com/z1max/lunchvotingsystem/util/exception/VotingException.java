ecb32e6bcbee322ce450b7edefd97580bdb637ca
z1max <forcazimaxqwe123>
2018-05-25 14:39:27 +0300

    Add voting exception

---
+++ src/main/java/com/z1max/lunchvotingsystem/util/exception/VotingException.java
@@ -0,0 +1,7 @@
+package com.z1max.lunchvotingsystem.util.exception;
+
+public class VotingException extends RuntimeException {
+    public VotingException(String message) {
+        super(message);
+    }
+}
